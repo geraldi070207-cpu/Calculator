3bafbb3bd00250c8d61dec7d8b2b0f0ca7fabf7f
Geraldi <geraldi070207@gmail.com>
2026-04-11 17:03:08 +0700

    Add Calculator project files

---
+++ Calculator/.history/Program_20260409083840.cs
@@ -0,0 +1,26 @@
+﻿// 25.61.0290 Geraldi
+
+int a = 10, b = 11;
+
+static int penjumlahan (int a, int b)
+{
+   return a + b;
+}
+static int pengurangan (int a, int b)
+{
+    return a - b;
+}
+static int perkalian (int a, int b)
+{
+    return a * b;
+}
+static int pembagian (int a, int b)
+{
+    return a / b;
+}
+
+
+Console.WriteLine(a.ToString()," + ",b," = "+ penjumlahan(a,b));
+Console.WriteLine(a.ToString()," - ",b," = "+ pengurangan(a,b));
+Console.WriteLine(a.ToString()," * ",b," = "+ perkalian(a,b));
+Console.WriteLine(a.ToString()," / ",b," = "+ pembagian(a,b));
---
+++ Calculator/.history/Program_20260409084047.cs
@@ -0,0 +1,26 @@
+﻿// 25.61.0290 Geraldi
+
+int a = 10, b = 11;
+
+static int penjumlahan (int a, int b)
+{
+   return a + b;
+}
+static int pengurangan (int a, int b)
+{
+    return a - b;
+}
+static int perkalian (int a, int b)
+{
+    return a * b;
+}
+static int pembagian (int a, int b)
+{
+    return a / b;
+}
+
+
+Console.WriteLine(a+" + "+b+" = "+ penjumlahan(a,b));
+Console.WriteLine(a+" - "+b+" = "+ pengurangan(a,b));
+Console.WriteLine(a+" * "+b+" = "+ perkalian(a,b));
+Console.WriteLine(a+" / "+b+" = "+ pembagian(a,b));
