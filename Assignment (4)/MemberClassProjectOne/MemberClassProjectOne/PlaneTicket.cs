b36a325c363727b2848a6134d6df750b89cb4de9
Geraldi <geraldi070207@gmail.com>
2026-04-24 03:37:57 +0700

    Upload Tugas PlaneTicket beneran

---
+++ Assignment (4)/MemberClassProjectOne/MemberClassProjectOne/PlaneTicket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberClassProjectOne
+{
+    internal class PlaneTicket
+    {
+        private string origin;
+        private string destination;
+        private int cost;
+        private string currency;
+
+        public string Origin
+        {
+            get { return origin; }
+            set { origin = value; }
+        }
+
+        public string Destination
+        {
+            get { return destination; }
+            set { destination = value; }
+        }
+        public int Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+        public string Currency
+        {
+            get { return currency; }
+            set { currency = value; }
+
+        }
+
+        public void PrintSummary()
+        {
+            if (currency == "USD")
+            {
+                Console.WriteLine($"Convert to Rupiah : {Cost * 15000}");
+            }
+            else 
+            {
+                Console.WriteLine($"Convert to Dolar : {Cost / 15000}");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Assignment (4)/MemberClassProjectOne/MemberClassProjectOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberClassProjectOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+
+            ticket1.Origin = "JOGJA";
+            ticket1.Destination = "PKU";
+            ticket1.Cost = 100;
+            ticket1.Currency = "USD";
+
+            Console.WriteLine($"Origin: {ticket1.Origin}");
+            Console.WriteLine($"Destination: {ticket1.Destination}");
+            Console.WriteLine($"Cost: {ticket1.Cost} ({ticket1.Currency})");
+            ticket1.PrintSummary();
+
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket2.Origin = "JAKARTA";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 750000;
+            ticket2.Currency = "IDR";
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Origin: {ticket2.Origin}");
+            Console.WriteLine($"Destination: {ticket2.Destination}");
+            Console.WriteLine($"Cost: {ticket2.Cost} ({ticket2.Currency})");
+            ticket2.PrintSummary();
+        }
+    }
+}
